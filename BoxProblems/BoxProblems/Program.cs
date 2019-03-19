53c7b40f74a8337f648a2a1899a9ce1afa5f6fb0
TheAIBot <andreasuranus@hotmail.com>
2019-03-19 16:08:53 +0100

    added way to show a state

--- BoxProblems/BoxProblems/Program.cs
+++ BoxProblems/BoxProblems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BoxProblems
 {
@@ -49,6 +50,71 @@ namespace BoxProblems
         public int PosToIndex(Point pos)
         {
             return pos.X + pos.Y * Width;
+        }
+
+        public string StateToString(State state)
+        {
+            char[][] world = new char[Height][];
+            for (int i = 0; i < Height; i++)
+            {
+                world[i] = new char[Width];
+            }
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (Walls[x, y])
+                    {
+                        world[y][x] = '#';
+                    }
+                    else
+                    {
+                        world[y][x] = ' ';
+                    }
+                }
+            }
+
+            foreach (var goal in Goals)
+            {
+                int x = goal.Pos.X;
+                int y = goal.Pos.Y;
+                int type = goal.Type;
+
+                world[y][x] = (char)('a' + type);
+            }
+
+            foreach (var agent in state.GetAgents(this))
+            {
+                int x = agent.Pos.X;
+                int y = agent.Pos.Y;
+                int type = agent.Type;
+
+                world[y][x] = (char)('0' + type);
+            }
+
+            foreach (var box in state.GetBoxes(this))
+            {
+                int x = box.Pos.X;
+                int y = box.Pos.Y;
+                int type = box.Type;
+
+                world[y][x] = (char)('A' + type);
+            }
+
+            StringBuilder sBuilder = new StringBuilder();
+            foreach (var worldRow in world)
+            {
+                sBuilder.Append(worldRow);
+                sBuilder.Append(Environment.NewLine);
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public override string ToString()
+        {
+            return StateToString(InitialState);
         }
     }
 

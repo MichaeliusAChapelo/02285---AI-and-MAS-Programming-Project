7c198a61bee9579dbfb5ca6997c9c88ddf89cd01
Michaelius A'Chapelo <mtopedo@hotmail.com>
2019-03-19 16:25:52 +0100

    Read Level function

--- BoxProblems/BoxProblems/Program.cs
+++ BoxProblems/BoxProblems/Program.cs
@@ -1,19 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BoxProblems
 {
     internal readonly struct Goal
     {
         public readonly Point Pos;
-        public readonly int Type;
+        public readonly char Type;
+
+        public Goal(Point Pos, char Type)
+        {
+            this.Pos = Pos;
+            this.Type = Type;
+        }
     }
 
     internal readonly struct Entity
     {
         public readonly Point Pos;
         public readonly int Color;
-        public readonly int Type;
+        public readonly char Type;
+
+        public Entity(Point Pos, int Color, char Type)
+        {
+            this.Pos = Pos;
+            this.Color = Color;
+            this.Type = Type;
+        }
     }
 
     internal class State
@@ -22,25 +39,135 @@ namespace BoxProblems
         Entity[] entities;
         //Command CMD;
         int G;
+
+        public State(State Parent, Entity[] entities, int G)
+        {
+            this.Parent = Parent;
+            this.entities = entities;
+            this.G = G;
+        }
     }
 
     internal class Level
     {
-        public readonly byte[,] Walls;
+        public readonly bool[,] Walls;
         public readonly Goal[] Goals;
         public readonly State InitialState;
         public readonly int Width;
         public readonly int Height;
         public readonly int BoxCount;
         public readonly int AgentCount;
+
+        public Level(bool[,] Walls, Goal[] Goals, State InitialState, int Width, int Height, int BoxCount, int AgentCount)
+        {
+            this.Walls = Walls;
+            this.Goals = Goals;
+            this.InitialState = InitialState;
+            this.Width = Width;
+            this.Height = Height;
+            this.BoxCount = BoxCount;
+            this.AgentCount = AgentCount;
+        }
+
+
+        public static Level ReadLevel(string path)
+        {
+            List<Entity> agents = new List<Entity>();
+            List<Entity> boxes = new List<Entity>();
+            List<Goal> goals = new List<Goal>();
+
+            var file = new System.IO.StreamReader(path);
+            // Keep all lines to ensure we only need read the level once.
+            string[] lines = File.ReadAllLines(path);
+
+            // Find indexes for levels and goals.
+            int initialLevelIndex = 0, goalLevelIndex = 0;
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                if (lines[i] == "#initial")
+                    initialLevelIndex = i + 1;
+                else if (lines[i] == "#goal")
+                {
+                    goalLevelIndex = i + 1;
+                    break;
+                }
+            }
+
+            // Define width/height of wall map.
+            int width = lines[initialLevelIndex].Length;
+            int height = goalLevelIndex - initialLevelIndex - 1;
+            bool[,] walls = new bool[width, height];
+
+
+            // Fix 'em colours (yes, elegant, European, superior and majestic British spelling)
+
+            int[] agentColours = new int[10];
+            int[] boxColours = new int[30];
+            for (int i = 5; i < initialLevelIndex - 1; ++i)
+            {
+                string[] contents = lines[i].Split(", ");
+                contents[0] = (contents[0]).Substring(contents[0].Length - 1);
+
+                for (int j = 0; j < contents.Length; ++j)
+                {
+                    if (Regex.IsMatch(contents[j], @"[0-9]"))
+                    {
+                        int num = int.Parse(contents[j]);
+                        agentColours[num] = i - 5; // Index of colour in text file.
+                    }
+
+                    else if (Regex.IsMatch(contents[j], @"[A-Z]"))
+                    {
+                        agentColours[char.Parse(contents[j]) - 'A'] = i - 5; // Index of colour in text file.
+                    }
+                }
+            }
+
+
+            // Read each line and each character.
+            for (int y = 0; y < goalLevelIndex - initialLevelIndex - 1; ++y)
+                for (int x = 0; x < lines[initialLevelIndex + y].Length; ++x)
+                {
+                    char c = (lines[initialLevelIndex + y])[x]; // Initial state character.
+
+                    if (c == '+') // Wall
+                        walls[x, y] = true;
+
+                    // Goals
+                    else if ((lines[goalLevelIndex + y])[x] != c
+                        && Regex.IsMatch(c.ToString(), @"[A-Z]"))
+                        goals.Add(new Goal(new Point(x, y), (lines[goalLevelIndex + y])[x]));
+
+                    // Efficiency!
+                    else if (c == ' ')
+                        continue;
+
+                    // Agent SPOTTED
+                    else if (Regex.IsMatch(c.ToString(), @"[0-9]"))
+                        agents.Add(new Entity(new Point(x, y), agentColours[c - '0'], c)); // Fix me l8 m8 gr8 1 m8
+
+                    // And another one of them boxes givin' us me problema.
+                    else if (Regex.IsMatch(c.ToString(), @"[A-Z]"))
+                        boxes.Add(new Entity(new Point(x, y), boxColours[c - 'A'], c)); // Fix me l8
+
+                    else throw new Exception("bruh i ain't findin no suitable charz brah");
+                }
+
+            agents.Sort((x, y) => x.Type.CompareTo(y.Type));
+
+            State state = new State(null, agents.Concat(boxes).ToArray(), 0);
+            return new Level(walls, goals.ToArray(), state, width, height, boxes.Count, agents.Count);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Level level = Level.ReadLevel("MAExample.lvl");
             Console.WriteLine("Hello World!");
             Console.Read();
+
         }
     }
 }

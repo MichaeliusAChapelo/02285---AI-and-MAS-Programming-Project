c5307dd11cad8cab596fe21fc862c46f3ef37fc6
TheAIBot <andreasuranus@hotmail.com>
2019-04-08 04:20:54 +0200

    add way to merge nodes that are fully connected

--- BoxProblems/BoxProblems/Graphing/Graph.cs
+++ BoxProblems/BoxProblems/Graphing/Graph.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace BoxProblems.Graphing
 {
     internal class Graph<N, E>
     {
         public readonly List<Node<N, E>> Nodes = new List<Node<N, E>>();
+        private static int asdsa = 0;
 
         protected void AddNode(Node<N, E> node)
         {
@@ -17,11 +19,14 @@ namespace BoxProblems.Graphing
         {
             StringBuilder nodesBuilder = new StringBuilder();
             StringBuilder edgesBuilder = new StringBuilder();
+            int nodes = 0;
+            int edges = 0;
 
             HashSet<Node<N, E>> foundNodes = new HashSet<Node<N, E>>();
             for (int i = 0; i < Nodes.Count; i++)
             {
-                nodesBuilder.Append($"{{ data: {{ id: '{i}', label: '{Nodes[i]}' }} }},");
+                nodesBuilder.Append($"{{ data: {{ id: '{asdsa + i}', label: '{Nodes[i]}' }} }},");
+                nodes++;
             }
             for (int i = 0; i < Nodes.Count; i++)
             {
@@ -32,11 +37,81 @@ namespace BoxProblems.Graphing
                         continue;
                     }
                     foundNodes.Add(Nodes[i]);
-                    edgesBuilder.Append($"{{ data: {{ source: '{i}', target: '{Nodes.IndexOf(edge.End)}' }} }},");
+                    edgesBuilder.Append($"{{ data: {{ source: '{asdsa + i}', target: '{asdsa + Nodes.IndexOf(edge.End)}' }} }},");
+                    edges++;
+                }
+            }
+            asdsa += Nodes.Count;
+            Console.WriteLine($"Nodes: {nodes}");
+            Console.WriteLine($"Edges: {edges}");
+            return (nodesBuilder.ToString(), edgesBuilder.ToString());
+        }
+
+        public static Graph<NodeGroup<N, E>, E> CreateSimplifiedGraph<N, E>(Graph<N, E> graph) where E : new()
+        {
+            var groupedGraph = new Graph<NodeGroup<N, E>, E>();
+            List<NodeGroup<N, E>> groups = new List<NodeGroup<N, E>>();
+            Dictionary<Node<N, E>, NodeGroup<N, E>> nodeToGroup = new Dictionary<Node<N, E>, NodeGroup<N, E>>(); 
+            foreach (var node in graph.Nodes)
+            {
+                var equalGroups = groups.Where(x => node.Edges.Count == x.EdgesTo.Count - 1 && node.Edges.All(y => x.EdgesTo.Contains(y.End)));
+                if (equalGroups.Count() == 0)
+                {
+                    var newGroup = new NodeGroup<N, E>(true);
+                    groups.Add(newGroup);
+
+                    newGroup.Nodes.Add(node);
+                    newGroup.EdgesTo.AddRange(node.Edges.Select(x => x.End));
+                    newGroup.EdgesTo.Add(node);
+                    nodeToGroup.Add(node, newGroup);
+                }
+                else
+                {
+                    equalGroups.First().Nodes.Add(node);
+                    nodeToGroup.Add(node, equalGroups.First());
                 }
             }
 
-            return ($"[{nodesBuilder.ToString()}]", $"[{edgesBuilder.ToString()}]");
+            Dictionary<NodeGroup<N, E>, Node<NodeGroup<N, E>, E>> groupToNode = new Dictionary<NodeGroup<N, E>, Node<NodeGroup<N, E>, E>>();
+            foreach (var group in groups)
+            {
+                var newNode = new Node<NodeGroup<N, E>, E>(group);
+                groupedGraph.AddNode(newNode);
+                groupToNode.Add(group, newNode);
+            }
+
+            foreach (var groupNode in groupedGraph.Nodes)
+            {
+                HashSet<Node<NodeGroup<N, E>, E>> alreadyCreatedEdges = new HashSet<Node<NodeGroup<N, E>, E>>();
+                foreach (var edgeNode in groupNode.Value.EdgesTo)
+                {
+                    var edgeGroupNode = groupToNode[nodeToGroup[edgeNode]];
+                    if (!alreadyCreatedEdges.Contains(edgeGroupNode) && groupNode != edgeGroupNode)
+                    {
+                        groupNode.AddEdge(new Edge<NodeGroup<N, E>, E>(edgeGroupNode, new E()));
+                        alreadyCreatedEdges.Add(edgeGroupNode);
+                    }
+                }
+            }
+
+            return groupedGraph;
+        }
+    }
+
+    internal readonly struct NodeGroup<N, E>
+    {
+        public readonly List<Node<N, E>> Nodes;
+        public readonly List<Node<N, E>> EdgesTo;
+
+        public NodeGroup(bool _)
+        {
+            this.Nodes = new List<Node<N, E>>();
+            this.EdgesTo = new List<Node<N, E>>();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", Nodes);
         }
     }
 }
--- BoxProblems/BoxProblems/Program.cs
+++ BoxProblems/BoxProblems/Program.cs
@@ -80,18 +80,26 @@ namespace BoxProblems
 
             ////Level level = Level.ReadLevel(File.ReadAllLines("Levels/New_Format/SplitExample2.lvl"));
             //Level level = Level.ReadOldFormatLevel(levelString.Replace("\r", "").Split('\n'), "asdas");// File.ReadAllLines("Levels/Old_Format/initial_levels/SAtowersOfSaigon10.lvl"), "asdas");
+
             Level wholeLevel = Level.ReadOldFormatLevel(File.ReadAllLines("Levels/Old_Format/real_levels/MAKarlMarx.lvl"), "asdas");
-            List<Level> levels = LevelSplitter.SplitLevel(wholeLevel);
-            foreach (var level in levels)
-            {
-                Console.WriteLine(level);
-                GoalGraph graph = new GoalGraph(level.InitialState, level);
-                //GraphShower.ShowGraph(graph);
-                var priority = new GoalPriority(level, graph);
-                Console.WriteLine(priority);
-                Console.WriteLine();
-                Console.WriteLine();
-            }
+            //Level wholeLevel = Level.ReadOldFormatLevel(File.ReadAllLines("Levels/Old_Format/initial_levels/SACrunch.lvl"), "asdas");
+            //Level wholeLevel = Level.ReadLevel(File.ReadAllLines("Levels/New_Format/SplitExample1.lvl"));
+            var goalGraphs = LevelSplitter.SplitLevel(wholeLevel).Select(x => new GoalGraph(x.InitialState, x))
+                                                                 .Select(x => Graph<GoalNode, GoalEdge>.CreateSimplifiedGraph(x))
+                                                                 .ToArray();
+            GraphShower.ShowGraphs(goalGraphs);
+
+            //List<Level> levels = LevelSplitter.SplitLevel(wholeLevel);
+            //foreach (var level in levels)
+            //{
+            //    Console.WriteLine(level);
+            //    GoalGraph graph = new GoalGraph(level.InitialState, level);
+            //    //GraphShower.ShowGraph(graph);
+            //    var priority = new GoalPriority(level, graph);
+            //    Console.WriteLine(priority);
+            //    Console.WriteLine();
+            //    Console.WriteLine();
+            //}
             Console.Read();
         }
 

25a59ae7976540e0fe78df27ea8f6c317624963c
TheAIBot <andreasuranus@hotmail.com>
2019-04-14 06:59:44 +0200

    add boxrunner project

---
+++ BoxProblems/BoxRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using BoxProblems;
+using System;
+
+namespace BoxRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdasda");
+            ProblemSolver.GetSolveStatistics("Levels/Old_Format/real_levels/SACybot.lvl", TimeSpan.MaxValue, false);
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}

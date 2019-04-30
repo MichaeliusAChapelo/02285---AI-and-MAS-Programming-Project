76b2974105188045e65cd68d082bb4ea56da3a2f
TheAIBot <andreasuranus@hotmail.com>
2019-04-30 10:40:18 +0200

    only need to specify the level name instead of the whole path

--- BoxProblems/BoxRunner/Program.cs
+++ BoxProblems/BoxRunner/Program.cs
@@ -1,15 +1,37 @@
 ﻿using BoxProblems;
 using BoxProblems.Solver;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace BoxRunner
 {
     class Program
     {
+        private static List<string> GetFilePathsFromFolderRecursively(string folderPath)
+        {
+            List<string> filepaths = new List<string>();
+            filepaths.AddRange(Directory.GetFiles(folderPath));
+
+            foreach (var direcotry in Directory.GetDirectories(folderPath))
+            {
+                filepaths.AddRange(GetFilePathsFromFolderRecursively(direcotry));
+            }
+
+            return filepaths;
+        }
+
+        private static string GetLevelPath(string levelFileName)
+        {
+            List<string> files = GetFilePathsFromFolderRecursively("Levels");
+            return files.Single(x => Path.GetFileName(x) == levelFileName);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Started");
-            var solution = ProblemSolver.SolveLevel("Levels/Old_Format/initial_levels/SACrunch.lvl", TimeSpan.FromHours(1), false);
+            var solution = ProblemSolver.SolveLevel(GetLevelPath("MATALK.lvl"), TimeSpan.FromHours(1), false);
             Console.WriteLine("Done");
             Console.Read();
         }

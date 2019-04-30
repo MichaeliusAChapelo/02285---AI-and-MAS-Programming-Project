21587a434679a8acbf7db1f6d24c841feef41605
Michaelius A'Chapelo <s143049@student.dtu.dk>
2019-04-30 17:27:40 +0200

    all the fixes

--- BoxProblems/BoxRunner/Program.cs
+++ BoxProblems/BoxRunner/Program.cs
@@ -1,39 +1,38 @@
 ﻿using BoxProblems;
+using BoxProblems.Graphing;
 using BoxProblems.Solver;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace BoxRunner
 {
     class Program
     {
-        private static List<string> GetFilePathsFromFolderRecursively(string folderPath)
-        {
-            List<string> filepaths = new List<string>();
-            filepaths.AddRange(Directory.GetFiles(folderPath));
-
-            foreach (var direcotry in Directory.GetDirectories(folderPath))
-            {
-                filepaths.AddRange(GetFilePathsFromFolderRecursively(direcotry));
-            }
-
-            return filepaths;
-        }
-
-        private static string GetLevelPath(string levelFileName)
-        {
-            List<string> files = GetFilePathsFromFolderRecursively("Levels");
-            return files.Single(x => Path.GetFileName(x) == levelFileName);
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Started");
-            var solution = ProblemSolver.SolveLevel(GetLevelPath("MATALK.lvl"), TimeSpan.FromHours(1), false);
-            Console.WriteLine("Done");
-            Console.Read();
+            if (args.Length == 0)
+            {
+                new ServerCommunicator().Run(args);
+            }
+            else
+            {
+                ServerCommunicator.PrintMap(); // Michaelius: With the new solver, everything messes up if I don't print this. DON'T ASK, I DON'T KNOW WHY
+
+
+                string levelPath = ServerCommunicator.levelPath; // Go to ServerCommunicator to pick a level.
+
+                var result = ProblemSolver.SolveLevel(levelPath, TimeSpan.FromHours(1), false);
+
+                var superList = result.Select(x => x.solutionMovesParts).ToList();
+
+                //new ServerCommunicator(superList).NonAsyncSolve(); // Solve locally for debugging purposes
+                var fisk = new ServerCommunicator(superList);//.Run(args); // Uses heuristics to solve in server client.
+                fisk.NonAsyncSolve();
+
+                return;
+                // Michaelius ENDO
+            }
         }
     }
 }

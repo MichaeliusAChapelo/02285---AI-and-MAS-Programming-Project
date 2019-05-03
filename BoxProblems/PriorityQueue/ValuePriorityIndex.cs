16ac64c679bfd135995a0211c57323767217ccac
Michaelius A'Chapelo <s143049@student.dtu.dk>
2019-05-03 13:53:48 +0200

    Priority Queue ayy
    I like how AIBot's code is now registered as my addition in GitHub. LOL

---
+++ BoxProblems/PriorityQueue/ValuePriorityIndex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriorityQueue
+{
+    public readonly struct ValuePriorityIndex
+    {
+        internal readonly int Index;
+        internal readonly int Version;
+    }
+}

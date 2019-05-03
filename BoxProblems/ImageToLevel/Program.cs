ae51768752188f9f49a5273f0986260446bb0e28
Michaelius A'Chapelo <s143049@student.dtu.dk>
2019-05-03 17:41:47 +0200

    Added SALeo.lvl + Img2Lvl Converter

---
+++ BoxProblems/ImageToLevel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace ImageToLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string imagePath = @"C:\SALeo.bmp";
+            const string outputPath = @"C:\Meine Items\Coding Ambitions\8. Semester\t.txt";
+
+            Bitmap b = new Bitmap(imagePath);
+
+            string[] content = new string[50];
+
+            for (int y = 0; y < b.Height; y++)
+            {
+                char[] str = new char[50];
+                for (int x = 0; x < b.Width; x++)
+                    if (b.GetPixel(x, y).R == 0)
+                        str[x] = '+';
+                    else str[x] = 'L';
+                content[y] = new string(str);
+            }
+
+            File.WriteAllLines(outputPath, content);
+
+        }
+    }
+
+}

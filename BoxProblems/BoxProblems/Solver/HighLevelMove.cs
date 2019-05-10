765aec55fe18a40c8894ef74f4e696a35c543131
TheAIBot <andreasuranus@hotmail.com>
2019-05-10 18:00:22 +0200

    remove moveNumber from highlevelmove

--- BoxProblems/BoxProblems/Solver/HighLevelMove.cs
+++ BoxProblems/BoxProblems/Solver/HighLevelMove.cs
@@ -10,15 +10,13 @@ namespace BoxProblems.Solver
         internal Entity MoveThis;
         internal Point ToHere;
         internal Entity? UsingThisAgent;
-        internal int MoveNumber;
 
-        internal HighlevelMove(State state, Entity moveThis, Point toHere, Entity? usingThisAgent, int moveNumber)
+        internal HighlevelMove(State state, Entity moveThis, Point toHere, Entity? usingThisAgent)
         {
             this.CurrentState = state;
             this.MoveThis = moveThis;
             this.ToHere = toHere;
             this.UsingThisAgent = usingThisAgent;
-            this.MoveNumber = moveNumber;
         }
 
         public override string ToString()

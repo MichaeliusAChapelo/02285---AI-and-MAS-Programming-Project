3bac9fd6a874b646e35809dc6e3ce64ff8730668
TheAIBot <andreasuranus@hotmail.com>
2019-05-10 17:15:17 +0200

    starting to improve command parallizer

--- BoxProblems/BoxProblems/CommandParallelizer.cs
+++ BoxProblems/BoxProblems/CommandParallelizer.cs
@@ -6,9 +6,21 @@ namespace BoxProblems
 {
     public static class CommandParallelizer
     {
+        private readonly struct FreeTime
+        {
+            public readonly int StartTime;
+            public readonly int EndTime;
+
+            public FreeTime(int start, int end)
+            {
+                this.StartTime = start;
+                this.EndTime = end;
+            }
+        }
+
         public static string[] Parallelize(List<AgentCommands> allCommands, Level level)
         {
-            int[,] world = new int[level.Width, level.Height];
+            List<FreeTime>[,] world = new List<FreeTime>[level.Width, level.Height];
             Point[] agentPositions = new Point[level.AgentCount];
             List<string[]> parallelizedCommands = new List<string[]>();
 
@@ -18,14 +30,23 @@ namespace BoxProblems
                 agentPositions[i] = initialAgents[i].Pos;
             }
 
+            for (int y = 0; y < level.Height; y++)
+            {
+                for (int x = 0; x < level.Width; x++)
+                {
+                    world[x, y] = new List<FreeTime>();
+                    world[x, y].Add(new FreeTime(0, int.MaxValue));
+                }
+            }
+
             foreach (AgentCommands agentCommands in allCommands)
             {
                 foreach (AgentCommand command in agentCommands.Commands)
                 {
                     Point agentPos = agentPositions[agentCommands.AgentIndex];
-                    int time = world[agentPos.X, agentPos.Y];
-
                     Point newAgentPos = command.GetNextAgentPos(agentPos);
+
+                    int time = world[agentPos.X, agentPos.Y];
                     int newTime = Math.Max(world[agentPos.X, agentPos.Y], world[newAgentPos.X, newAgentPos.Y]);
 
                     if (command.CType != CommandType.MOVE)
@@ -68,6 +89,13 @@ namespace BoxProblems
             }
             
             return finishedCommands;
+        }
+
+        private static bool FindTimeRoute(List<AgentCommand> commands, int commandIndex, Point agentPos, int startTime, int currentEndTime)
+        {
+            AgentCommand command = commands[commandIndex];
+
+            Point newAgentPos = command.GetNextAgentPos(agentPos);
         }
     }
 }
